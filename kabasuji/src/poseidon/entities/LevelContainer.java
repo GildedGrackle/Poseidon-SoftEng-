be7b64a18e6acee815d986553d6dea38654577b1
Sheilakh <jacob.wennersten@sbcglobal.net>
2016-05-03 17:34:19 -0400

    Removed inGame attribute from levelContainer.

--- kabasuji/src/poseidon/entities/LevelContainer.java
+++ kabasuji/src/poseidon/entities/LevelContainer.java
@@ -8,8 +8,6 @@ package poseidon.entities;
 public class LevelContainer {
 	/** For file reading. */
 	String levelFileName;
-	/** If it is one of the original 15, custom, or not in the game but saved. */
-	int inGame;  // TODO create standard constants for this
 	/** Level for placement in level select screen and for game progress. */
 	int levelNumber;
 	/** The level associated with this container. */
@@ -24,14 +22,12 @@ public class LevelContainer {
 	 *  Constructor.
 	 *  
 	 *  @param levelFileName  name of the file containing the Level's data
-	 *  @param inGame  indicator of status: built-in, custom added, or unadded but saved
 	 *  @param lvlnum  Level number, for display on the Level Select screen
 	 *  @param level  the Level associated with this container
 	 *  @param score  the highest score attained on the Level
 	 */
-	public LevelContainer (String levelFileName, int inGame, int lvlnum, LevelModel level, int score) {
+	public LevelContainer (String levelFileName, int lvlnum, LevelModel level, int score) {
 		this.levelFileName = levelFileName;
-		this.inGame = inGame;
 		this.levelNumber = lvlnum;
 		this.level = level;
 		this.isChanged = false;				//Set to false by default
@@ -45,10 +41,6 @@ public class LevelContainer {
 	public String getLevelFileName()
 	{
 		return levelFileName;
-	}
-	public int getInGame()
-	{
-		return inGame;
 	}
 	public int getLevelNumber()
 	{
@@ -69,10 +61,6 @@ public class LevelContainer {
 	public void setLevelFileName(String levelFileName)
 	{
 		this.levelFileName = levelFileName;
-	}
-	public void setInGame(int inGame)
-	{
-		this.inGame = inGame;
 	}
 	public void setLevelNumber(int levelNumber)
 	{
