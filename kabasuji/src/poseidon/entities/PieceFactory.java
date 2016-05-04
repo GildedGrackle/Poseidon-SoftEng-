1d80e7245292bcfd78ad6f3de2822896aa32d073
Natasha <Natasha@130.215.224.73>
2016-05-03 21:20:08 -0400

    fixed double piece

--- kabasuji/src/poseidon/entities/PieceFactory.java
+++ kabasuji/src/poseidon/entities/PieceFactory.java
@@ -81,7 +81,7 @@ public class PieceFactory {
 				new Point(1, 1), new Point(2, 1), new Point(0, 2), new Point(1, 2)});
 		
 		pieces[21] = new Piece (new Point[] {new Point(1, 0), new Point(2, 0),
-				new Point(1, 1), new Point(2, 1), new Point(0, 2), new Point(1, 2)});	
+				new Point(1, 1), new Point(2, 1), new Point(0, 1), new Point(1, 2)});	
 		
 		pieces[22] = new Piece (new Point[] {new Point(0, 0), new Point(3, 0),
 				new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1)});
